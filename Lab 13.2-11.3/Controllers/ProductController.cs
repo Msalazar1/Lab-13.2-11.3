8b53443b4f052105eff7e6334d7506af67e3cc1b
Manny <mannysalazar@quickenloans.com>
2020-09-14 22:16:26 -0400

    Lab 13.2 and 11.3

---
+++ Lab 13.2-11.3/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+using Lab_13._2_11._3.Models;
+using Dapper;
+
+namespace Lab_13._2_11._3.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            IDbConnection db = new SqlConnection("Server=BW18Q13\\SQLEXPRESS;Database=CoffeeShop;user id=test;password=password");
+            db.Open();
+            List<string> ItemCategory = db.Query<string>("select distinct ItemCategory from ItemList order by ItemCategory desc").AsList<string>();
+            db.Close();
+
+            return View(ItemCategory);
+
+            //return View();
+        }
+
+        public IActionResult Coffees()
+        {
+            IDbConnection db = new SqlConnection("Server=BW18Q13\\SQLEXPRESS;Database=CoffeeShop;user id=test;password=password");
+            db.Open();
+            List<string> ItemName = db.Query<string>("select distinct ItemName from ItemList order by ItemName desc").AsList<string>();
+            db.Close();
+
+            return View(ItemName);
+        }
+
+        public IActionResult ProductInfo(int ItemID)
+        {
+            IDbConnection db = new SqlConnection("Server=BW18Q13\\SQLEXPRESS;Database=CoffeeShop;user id=test;password=password");
+            db.Open();
+            Product product = db.QuerySingle<Product>($"select * from ItemList where ItemID = {ItemID}");
+            db.Close();
+
+            return View(product);
+        }
+    }
+}
---
+++ Lab 13.2-11.3/Models/Product.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper.Contrib.Extensions;
+
+namespace Lab_13._2_11._3.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        public long id { get; set; }
+        
+        public string Category { get; set; }
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+        public Decimal Price { get; set; }
+
+        public static Product Create(string _name, Decimal _price)
+        {
+            //create the new Product instance
+            Product prod = new Product() { Name = _name, Price = _price };
+
+            //save it to the database
+            IDbConnection db = new SqlConnection("Server=BW18Q13\\SQLEXPRESS;Database=devbuildjoin;user id=test;password=password");
+            long _id = db.Insert<Product>(prod);
+
+            //set the id column of the instance
+            prod.id = _id;
+
+            //return the instance
+            return prod;
+        }
+        public static Product Read(long _id)
+        {
+            IDbConnection db = new SqlConnection("Server=BW18Q13\\SQLEXPRESS;Database=devbuildjoin;user id=test;password=password");
+            Product prod = db.Get<Product>(_id);
+            return prod;
+        }
+        public static List<Product> Read()
+        {
+            IDbConnection db = new SqlConnection("Server=BW18Q13\\SQLEXPRESS;Database=devbuildjoin;user id=test;password=password");
+            List<Product> prods = db.GetAll<Product>().ToList();
+            return prods;
+        }
+        public static void Delete(long _id)
+        {
+            IDbConnection db = new SqlConnection("Server=BW18Q13\\SQLEXPRESS;Database=devbuildjoin;user id=test;password=password");
+            db.Delete(new Product() { id = _id });
+
+        }
+        public void Save()
+        {
+            IDbConnection db = new SqlConnection("Server=BW18Q13\\SQLEXPRESS;Database=devbuildjoin;user id=test;password=password");
+            db.Update(this);
+        }
+    }
+}
